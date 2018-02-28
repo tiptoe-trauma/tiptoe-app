ac177d88e327234b231060bcff3e35804ae46490
Joseph Utecht <joseph.utecht@gmail.com>
2018-02-28 13:49:52 -0600

    hoverover for joyplot

--- src/app/org-joyplot/org-joyplot.component.ts
+++ src/app/org-joyplot/org-joyplot.component.ts
@@ -3,9 +3,12 @@ import { OrganogramService, JoyPlotNumbers } from '../organogram.service';
 import { Bar, BarChart } from '../compact-bar/compact-bar.component';
 
 export interface Path {
+  id: string;
   path: string;
   offset: string;
   color: string;
+  box_size: number;
+  boxes: {}[];
 }
 
 @Component({
@@ -19,10 +22,12 @@ export class OrgJoyplotComponent implements OnInit {
   public max_value: number;
   public paths: Path[];
   public show_bars: boolean;
+  public hovered: string;
 
   constructor(private _organogramService: OrganogramService) { }
 
   ngOnInit() {
+    this.hovered = "";
     this.show_bars = false;
     this.bars = [];
     this.paths = [];
@@ -40,11 +45,21 @@ export class OrgJoyplotComponent implements OnInit {
     this.show_bars = !this.show_bars;
   }
 
+  displayName(name: string){
+    this.hovered = name;
+  }
+
+  clearName(){
+    this.hovered = "";
+  }
+
   createPaths(joynums: JoyPlotNumbers[]){
     this.paths = [];
     let offset = 0;
     for(let jn of joynums){
       let path = <Path>{};
+      path.box_size = 5;
+      path.boxes = [];
       path.path = "M0 0";
       let x = 0;
       let lastvalue = 0;
@@ -52,18 +67,20 @@ export class OrgJoyplotComponent implements OnInit {
         if(typeof jn[key] == 'number'){
           let value = jn[key];
           if(value < this.max_value){
+            path.boxes.push({'name': key, 'x': x});
             path.path += (" Q " + x + ' ' + lastvalue);
-            x += 5;
+            x += path.box_size;
             let halfway = ((value - lastvalue)/2) + lastvalue;
             path.path += (", " + x + ' ' + halfway);
-            x += 5;
+            x += path.box_size;
             lastvalue = value;
           }
         }
       }
       path.path += " L" + x + " 0 L0 0";
       path.offset = "matrix(1 0 0 -1 0 " + (50 - offset)+ ")";
-      offset += 5;
+      path.id = "path" + offset;
+      offset += path.box_size;
       path.color = offset % 2 == 0 ? '#0064FF' : '#2B82FF';
       this.paths.push(path);
     }
