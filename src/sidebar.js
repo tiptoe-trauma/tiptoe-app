f83ea044364fa2a51c556090949cd1c709d23d4f
Braun <looseymoose@FT106-106-M2.local>
2020-05-14 10:44:51 -0400

    Prevent fixed position sidebar from overlapping footer

--- src/sidebar.js
+++ src/sidebar.js
@@ -15,19 +15,37 @@
 
 
 function showFigure() {
-    console.log("In function")
     var sidebar = document.getElementById("figure")
     if (sidebar.style.display === "block") {
         sidebar.style.display = "none";
-        sidebar.style.paddingTop = "0"
-        sidebar.style.transition = "0.5s"
+        sidebar.style.paddingTop = "0";
+        sidebar.style.transition = "0.5s";
+        document.getElementById("openbtn").innerHTML = "Show Comparison";
     } else {
         sidebar.style.display = "block";
-        sidebar.style.paddingTop = "60px"
-        sidebar.style.transition = "0.5s"
+        sidebar.style.paddingTop = "20px";
+        sidebar.style.transition = "0.5s";
+        document.getElementById("openbtn").innerHTML = "Hide Comparison";
     }
 }
 
-function test() {
-    document.getElementById("testbutton").innerText = "Bingo";
+window.onscroll = function adjustSidebar() {
+    var window_height = document.body.scrollHeight
+    var figure_size = $("#sidebarContent").prop("scrollHeight")
+
+    var cutoff = window_height - figure_size - 150
+
+    var content = document.getElementById("sidebarContent")
+    var current_pos = $("#sidebarContent").offset()
+    var current_offset = parseInt($("#sidebarContent").css("transform").split(',')[5]) 
+
+    if ((current_pos.top - current_offset) > cutoff) {
+        var offset_increase = cutoff - current_pos.top
+        var new_offset = current_offset + offset_increase
+        content.style.setProperty('transform', "translateY(" + new_offset + "px)");
+    } else {
+        content.style.setProperty('transform', "translateY(0px)");
+    }
+
+
 }
