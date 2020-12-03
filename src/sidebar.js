6b7f94451df9884316c5a985450ee2b0070c57c2
Braun <looseymoose@FT106-106-M2.local>
2020-12-02 20:21:29 -0500

    Only run sidebar function when a sidebar is present

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,32 +1,34 @@
 function adjustSidebar() {
-    var window_height = document.body.scrollHeight;
-    var figure_size = $("#sidebarContent").prop("scrollHeight");
-    var question_size = $(".questions").prop("scrollHeight");
-    var content = document.getElementById("sidebarContent");
+    if(document.getElementById("sidebarContent")){
+        var window_height = document.body.scrollHeight;
+        var figure_size = $("#sidebarContent").prop("scrollHeight");
+        var question_size = $(".questions").prop("scrollHeight");
+        var content = document.getElementById("sidebarContent");
 
-    var cutoff = window_height - figure_size - 150;
-    var current_pos = $("#sidebarContent").offset();
-    var question_pos = $(".questions").offset();
-    var current_offset = parseInt($("#sidebarContent").css("transform").split(',')[5]);
+        var cutoff = window_height - figure_size - 150;
+        var current_pos = $("#sidebarContent").offset();
+        var question_pos = $(".questions").offset();
+        var current_offset = parseInt($("#sidebarContent").css("transform").split(',')[5]);
 
-    if (figure_size > question_size){
-        content.style.setProperty("max-height", question_size + "px");
-        content.style.setProperty("max-width", "40%");
-        content.style.setProperty("overflow-y", "scroll");
+        if (figure_size > question_size){
+            content.style.setProperty("max-height", question_size + "px");
+            content.style.setProperty("max-width", "40%");
+            content.style.setProperty("overflow-y", "scroll");
 
-        var offset_increase = question_pos.top - current_pos.top;
-        var new_offset = current_offset + offset_increase;
-        content.style.setProperty('transform', "translateY(" + new_offset + "px)");
-    } else {
-        content.style.removeProperty("max-height");
-        content.style.removeProperty("max-width");
-        content.style.removeProperty("overflow-y");
-        if ((current_pos.top - current_offset) > cutoff) {
-            var offset_increase = cutoff - current_pos.top;
+            var offset_increase = question_pos.top - current_pos.top;
             var new_offset = current_offset + offset_increase;
             content.style.setProperty('transform', "translateY(" + new_offset + "px)");
         } else {
-            content.style.setProperty('transform', "translateY(0px)");
+            content.style.removeProperty("max-height");
+            content.style.removeProperty("max-width");
+            content.style.removeProperty("overflow-y");
+            if ((current_pos.top - current_offset) > cutoff) {
+                var offset_increase = cutoff - current_pos.top;
+                var new_offset = current_offset + offset_increase;
+                content.style.setProperty('transform', "translateY(" + new_offset + "px)");
+            } else {
+                content.style.setProperty('transform', "translateY(0px)");
+            }
         }
     }
 }
